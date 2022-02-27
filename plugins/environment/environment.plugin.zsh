654af7757ebc01c12bae964ee9424a8605868846
mattmc3 <mattmc3@gmail.com>
2022-02-26 21:31:31 -0500

    Add environment plugin

---
+++ plugins/environment/environment.plugin.zsh
@@ -0,0 +1,48 @@
+#
+# Set general shell options and defines environment variables
+#
+
+#
+# Smart URLs
+#
+
+# This logic comes from an old version of zim. Essentially, bracketed-paste was
+# added as a requirement of url-quote-magic in 5.1, but in 5.1.1 bracketed
+# paste had a regression. Additionally, 5.2 added bracketed-paste-url-magic
+# which is generally better than url-quote-magic so we load that when possible.
+autoload -Uz is-at-least
+if [[ ${ZSH_VERSION} != 5.1.1 && ${TERM} != "dumb" ]]; then
+  if is-at-least 5.2; then
+    autoload -Uz bracketed-paste-url-magic
+    zle -N bracketed-paste bracketed-paste-url-magic
+  elif is-at-least 5.1; then
+    autoload -Uz bracketed-paste-magic
+    zle -N bracketed-paste bracketed-paste-magic
+  fi
+  autoload -Uz url-quote-magic
+  zle -N self-insert url-quote-magic
+fi
+
+#
+# General
+#
+
+setopt COMBINING_CHARS       # combine zero-length punctuation characters (accents)
+                             #   with the base character
+setopt INTERACTIVE_COMMENTS  # enable comments in interactive shell
+setopt RC_QUOTES             # allow 'Henry''s Garage' instead of 'Henry'\''s Garage'
+unsetopt MAIL_WARNING        # don't print a warning message if a mail file has been accessed
+
+# Allow mapping Ctrl+S and Ctrl+Q shortcuts
+[[ -r ${TTY:-} && -w ${TTY:-} && $+commands[stty] == 1 ]] && stty -ixon <$TTY >$TTY
+
+#
+# Jobs
+#
+
+setopt LONG_LIST_JOBS  # List jobs in the long format by default
+setopt AUTO_RESUME     # Attempt to resume existing job before creating a new process
+setopt NOTIFY          # Report status of background jobs immediately
+unsetopt BG_NICE       # Don't run all background jobs at a lower priority
+unsetopt HUP           # Don't kill jobs on shell exit
+unsetopt CHECK_JOBS    # Don't report on jobs when shell exit
