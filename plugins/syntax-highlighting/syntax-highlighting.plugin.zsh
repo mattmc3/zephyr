e2bfa8eb031f57b091f00f736f2c79bf7a10c5ac
mattmc3 <mattmc3@gmail.com>
2022-02-27 13:20:30 -0500

    Add zstyle for customizing syntax highlighting

--- plugins/syntax-highlighting/syntax-highlighting.plugin.zsh
+++ plugins/syntax-highlighting/syntax-highlighting.plugin.zsh
@@ -2,6 +2,11 @@
 # External
 #
 
--zephyr-clone-subplugin syntax-highlighting zsh-users/zsh-syntax-highlighting
--zephyr-clone-subplugin syntax-highlighting zdharma-continuum/fast-syntax-highlighting
-source ${0:A:h}/external/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+# zstyle ':zephyr:plugin:syntax-highlighting' use-fast-syntax-highlighting 'yes'
+if zstyle -t ':zephyr:plugin:syntax-highlighting' use-fast-syntax-highlighting; then
+  -zephyr-clone-subplugin syntax-highlighting zdharma-continuum/fast-syntax-highlighting
+  source ${0:A:h}/external/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+else
+  -zephyr-clone-subplugin syntax-highlighting zsh-users/zsh-syntax-highlighting
+  source ${0:A:h}/external/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fi
