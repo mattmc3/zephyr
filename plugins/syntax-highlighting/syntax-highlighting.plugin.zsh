e72bc7069e462d58ef4b91438ba63a22e5903a81
mattmc3 <mattmc3@gmail.com>
2022-02-26 21:34:02 -0500

    Add syntax-highlighting plugin

---
+++ plugins/syntax-highlighting/syntax-highlighting.plugin.zsh
@@ -0,0 +1,7 @@
+#
+# External
+#
+
+zephyr-clone-external syntax-highlighting zsh-users/zsh-syntax-highlighting
+zephyr-clone-external syntax-highlighting zdharma-continuum/fast-syntax-highlighting
+source ${0:A:h}/external/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
