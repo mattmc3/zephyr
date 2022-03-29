86a1f7b09a3a85ac3d86a12ee589a8ae18bbca3a
mattmc3 <mattmc3@gmail.com>
2022-03-29 08:34:01 -0400

    Add external prompts

--- plugins/prompt/prompt.plugin.zsh
+++ plugins/prompt/prompt.plugin.zsh
@@ -12,6 +12,32 @@ setopt PROMPT_SUBST  # expand parameters in prompt variables
 
 #endregion
 
+#region: External
+
+typeset -A _external_prompts=(
+  sindresorhus/pure      pure
+  romkatv/powerlevel10k  powerlevel10k
+  dracula/zsh            dracula
+)
+for _repo _prompt_name in ${(kv)_external_prompts}; do
+  if [[ ! -d "${0:A:h}/external/$_prompt_name" ]]; then
+    _prompt_dir="${0:A:h}/external/$_prompt_name"
+    command git clone --quiet --depth 1 \
+      https://github.com/$_repo \
+      "$_prompt_dir"
+    _prompt_init="${_prompt_dir}/prompt_${_prompt_name}_setup"
+    if [[ ! -e "$_prompt_init" ]]; then
+      _prompt_files=("$_prompt_dir"/prompt_*_setup(.N) "$_prompt_dir"/*.zsh-theme(.N))
+      [[ ${#_prompt_files[@]} -gt 0 ]] && ln -sf "${_prompt_files[1]}" "$_prompt_init"
+    fi
+    unset _prompt_{dir,init,files}
+  fi
+  fpath+=("${0:A:h}/external/$_prompt_name")
+done
+unset _external_prompts _repo _prompt_name
+
+#endregion
+
 #region: Init
 
 autoload -Uz promptinit && promptinit
