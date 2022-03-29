fcad152dce7f216d789b1d267eb74832f3fc95ba
mattmc3 <mattmc3@gmail.com>
2022-03-29 09:27:36 -0400

    Make sure ZEPHYR_HOME is set

--- zephyr.zsh
+++ zephyr.zsh
@@ -1,4 +1,5 @@
 0=${(%):-%x}
+ZEPHYR_HOME=${ZEPHYR_HOME:-${0:A:h}}
 
 # this file should contain the bare minimum to bootstrap Zephyr so that plugins can
 # remain independant
@@ -19,6 +20,6 @@ zstyle -a ':zephyr:load' plugins \
   '_zephyr_plugins' || _zephyr_plugins=($_zephyr_plugins_default)
 
 for _zephyr_plugin in $_zephyr_plugins; do
-  source ${0:A:h}/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh
+  source $ZEPHYR_HOME/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh
 done
 unset _zephyr_plugin{s,s_default,}
