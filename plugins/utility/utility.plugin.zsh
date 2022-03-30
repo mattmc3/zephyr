7ebb01bee14f116c437694fc94e9005d0427dedc
mattmc3 <mattmc3@gmail.com>
2022-03-30 13:28:09 -0400

    Move MacOS plugin into utility

--- plugins/utility/utility.plugin.zsh
+++ plugins/utility/utility.plugin.zsh
@@ -91,6 +91,50 @@ alias pbp='pbpaste'
 
 #endregion
 
+#region: Extended tools
+
+# OS-specific tools
+if [[ "$OSTYPE" == darwin* ]]; then
+  # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/macos
+  # https://github.com/sorin-ionescu/prezto/tree/master/modules/osx
+  autoload-dir "${0:A:h}/functions-macos"
+
+  # homebrew
+  if (( $+commands[brew] )); then
+    autoload-dir "${0:A:h}/functions-macos/homebrew"
+  fi
+
+  # Dash.app
+  autoload-dir "${0:A:h}/functions-macos/dash"
+
+  # aliases
+  alias showfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+  alias hidefiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+  # flush DNS
+  alias flushdns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+
+  # finder integrations
+  alias ofd='open "$PWD"'
+
+  # changes directory to the current Finder directory
+  alias cdf='cd "$(pfd)"'
+
+  # pushes directory to the current Finder directory
+  alias pushdf='pushd "$(pfd)"'
+
+  # canonical hex dump; some systems have this symlinked
+  command -v hd > /dev/null || alias hd="hexdump -C"
+
+  # MacOS has no 'md5sum', so use 'md5' as a fallback
+  command -v md5sum > /dev/null || alias md5sum="md5"
+
+  # MacOS has no 'sha1sum', so use 'shasum' as a fallback
+  command -v sha1sum > /dev/null || alias sha1sum="shasum"
+fi
+
+#endregion
+
 #region: Cleanup
 
 unfunction -- -coreutils-alias-setup
