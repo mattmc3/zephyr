74a188bb36208d0ad4076342a1719ba3f560427f
mattmc3 <mattmc3@gmail.com>
2022-06-30 17:28:37 -0400

    Simplify zephyr plugin init

--- zephyr.zsh
+++ zephyr.zsh
@@ -1,26 +1,5 @@
 0=${(%):-%x}
-ZEPHYR_HOME=${ZEPHYR_HOME:-${0:A:h}}
-source $ZEPHYR_HOME/lib/init.zsh
-
-_zephyr_plugins_default=(
-  environment
-  terminal
-  editor
-  history
-  directory
-  utility
-  zfunctions
-  confd
-  prompt
-  completions
-  autosuggestions
-  syntax-highlighting
-  history-substring-search
-)
-zstyle -a ':zephyr:load' plugins \
-  '_zephyr_plugins' || _zephyr_plugins=($_zephyr_plugins_default)
-
-for _zephyr_plugin in $_zephyr_plugins; do
-  source $ZEPHYR_HOME/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh
+for _zephyr_plugin in "${0:A:h}"/plugins/*/*.plugin.zsh; do
+  source $_zephyr_plugin
 done
-unset _zephyr_plugin{s,s_default,}
+unset _zephyr_plugin
