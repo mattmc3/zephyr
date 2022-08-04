36f14acf8afe0026c1be75324d95b96f82b42af6
mattmc3 <mattmc3@gmail.com>
2022-08-04 10:31:45 -0400

    Update environment plugin

--- plugins/environment/environment.plugin.zsh
+++ plugins/environment/environment.plugin.zsh
@@ -1,18 +1,7 @@
 #
-# Set shell options and setup environment.
+# Options
 #
 
-#region: Smart URLs
-if ${TERM} != "dumb" ]]; then
-  autoload -Uz bracketed-paste-url-magic
-  zle -N bracketed-paste bracketed-paste-url-magic
-  autoload -Uz url-quote-magic
-  zle -N self-insert url-quote-magic
-fi
-#endregion
-
-#region: Options
-# general options
 setopt COMBINING_CHARS       # combine zero-length punctuation characters (accents)
                              #   with the base character
 setopt INTERACTIVE_COMMENTS  # enable comments in interactive shell
@@ -30,45 +19,11 @@ setopt NOTIFY                # report status of background jobs immediately
 unsetopt BG_NICE             # don't run all background jobs at a lower priority
 unsetopt HUP                 # don't kill jobs on shell exit
 unsetopt CHECK_JOBS          # don't report on jobs when shell exit
-#endregion
 
-#region: Aliases
-alias type="type -a"
-alias mkdir="mkdir -p"
-GREP_EXCL=(.bzr CVS .git .hg .svn .idea .tox)
-alias grep="${aliases[grep]:-grep} --exclude-dir={\${(j.,.)GREP_EXCL}}"
+#
+# Variables
+#
 
-# macOS utils everywhere
-if [[ "$OSTYPE" == darwin* ]]; then
-  alias o='open'
-elif [[ "$OSTYPE" == cygwin* ]]; then
-  alias o='cygstart'
-  alias pbcopy='tee > /dev/clipboard'
-  alias pbpaste='cat /dev/clipboard'
-elif [[ "$OSTYPE" == linux-android ]]; then
-  alias o='termux-open'
-  alias pbcopy='termux-clipboard-set'
-  alias pbpaste='termux-clipboard-get'
-else
-  alias o='xdg-open'
-  if [[ -n $DISPLAY ]]; then
-    if (( $+commands[xclip] )); then
-      alias pbcopy='xclip -selection clipboard -in'
-      alias pbpaste='xclip -selection clipboard -out'
-    elif (( $+commands[xsel] )); then
-      alias pbcopy='xsel --clipboard --input'
-      alias pbpaste='xsel --clipboard --output'
-    fi
-  else
-    if (( $+commands[wl-copy] && $+commands[wl-paste] )); then
-      alias pbcopy='wl-copy'
-      alias pbpaste='wl-paste'
-    fi
-  fi
-fi
-#endregion
-
-#region: Variables
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
@@ -83,9 +38,21 @@ if [[ "$OSTYPE" == darwin* ]]; then
   export XDG_VIDEOS_DIR=${XDG_VIDEOS_DIR:-$HOME/Videos}
   export XDG_PROJECTS_DIR=${XDG_PROJECTS_DIR:-$HOME/Projects}
 fi
-#endregion
 
-#region: Help
-# Load more specific 'run-help' function from $fpath.
-(( $+aliases[run-help] )) && unalias run-help && autoload -Uz run-help
-#endregion
+#
+# Aliases
+#
+
+GREP_EXCL=(.bzr CVS .git .hg .svn .idea .tox)
+alias grep="${aliases[grep]:-grep} --exclude-dir={\${(j.,.)GREP_EXCL}}"
+
+#
+# Init
+#
+
+if ${TERM} != "dumb" ]]; then
+  autoload -Uz bracketed-paste-url-magic
+  zle -N bracketed-paste bracketed-paste-url-magic
+  autoload -Uz url-quote-magic
+  zle -N self-insert url-quote-magic
+fi
