7982c9c6f62ebaab50ca7a98890a3d3747239fda
mattmc3 <mattmc3@gmail.com>
2022-12-26 11:02:42 -0500

    Change directory plugin

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -4,11 +4,12 @@
 
 # Directory options
 setopt AUTO_PUSHD            # cd automatically uses the dirstack.
-setopt PUSHD_IGNORE_DUPS     # No duplicates in the dirstack.
+setopt CDABLE_VARS           # Change directory to a path stored in a variable.
+setopt EXTENDED_GLOB         # Use extended globbing syntax.
+setopt MULTIOS               # Write to multiple descriptors.
 setopt PUSHD_MINUS           # Swap meanings of +/- to be more natural.
 setopt PUSHD_SILENT          # Do not print the dirstack after pushd/popd.
 setopt PUSHD_TO_HOME         # pushd with no args goes to home.
-setopt CDABLE_VARS           # Change directory to a path stored in a variable.
 
 #
 # Aliases
@@ -24,7 +25,3 @@ alias dirh='dirs -v'
     alias -g ..${idx}="${(j./.)dotdot}"
   done
 }
-
-# set initial working directory
-IWD=${IWD:-PWD}
-alias iwd='cd $IWD'
