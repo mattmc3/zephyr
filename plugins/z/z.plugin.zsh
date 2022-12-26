ebbd1e58f006a11de5837e6cfb73f877b62913cd
mattmc3 <mattmc3@gmail.com>
2022-12-26 16:30:17 -0500

    Add z plugin

---
+++ plugins/z/z.plugin.zsh
@@ -0,0 +1,21 @@
+###
+# z - Jump around with rupa/z.
+###
+
+#
+# Requirements
+#
+
+0=${(%):-%x}
+: ${ZEPHYR_HOME:=${0:A:h:h:h}}
+zstyle -t ':zephyr:core' initialized || . $ZEPHYR_HOME/lib/init.zsh
+
+#
+# Init
+#
+
+source $ZEPHYR_HOME/plugins/.external/z/z.sh
+if [[ -z "$_Z_DATA" ]]; then
+  _Z_DATA=${XDG_DATA_HOME:=$HOME/.local/share}/z/data
+fi
+[[ -f $_Z_DATA ]] || { mkdir -p ${_Z_DATA:h} && touch $_Z_DATA }
