cc5734621e888ab8ad4a4c5d7ab844868f44e074
mattmc3 <mattmc3@gmail.com>
2022-12-26 22:25:47 -0500

    Update zephyr init process

--- zephyr.zsh
+++ zephyr.zsh
@@ -5,21 +5,39 @@
 ZEPHYR_HOME=${ZEPHYR_HOME:-${0:A:h}}
 source $ZEPHYR_HOME/lib/init.zsh
 
-# load plugins
-_zephyr_plugins_default=(
-  colors
-  environment
-  editor
-  history
-  directory
-  utility
-  prompt
-  $ZEPHYR_HOME/.external
-)
-zstyle -a ':zephyr:load' plugins \
-  '_zephyr_plugins' || _zephyr_plugins=($_zephyr_plugins_default)
+# allow OMZ plugins
+ZSH=$ZEPHYR_HOME/.external/ohmyzsh
+ZSH_CUSTOM="${ZSH_CUSTOM:-${ZDOTDIR:-${XDG_CONFIG_HOME:-$HOME/.config}/zsh}}"
 
+# get list of plugins from zstyle or plugins variable
+zstyle -a ':zephyr:load' plugins \
+  '_zephyr_plugins' || _zephyr_plugins=($plugins)
+
+# if nothing provided, use the default list
+(( $#_zephyr_plugins )) ||
+  _zephyr_plugins=(
+    colors
+    environment
+    editor
+    history
+    directory
+    utility
+    prompt
+    completions
+  )
+
+# Load plugins.
 for _zephyr_plugin in $_zephyr_plugins; do
-  source $ZEPHYR_HOME/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh
+  _initfiles=(
+    $ZSH_CUSTOM/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh(N)
+    $ZEPHYR_HOME/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh(N)
+    $ZSH/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh(N)
+  )
+  if (( $#_initfiles )); then
+    echo "Loading plugin $_zephyr_plugin from $_initfiles[1]"
+    source "$_initfiles[1]"
+  else
+    echo >&2 "zephyr: Plugin not found '$_zephyr_plugin'."
+  fi
 done
-unset _zephyr_plugin{s,s_default,}
+unset _zephyr_plugin{s,} _initfiles
