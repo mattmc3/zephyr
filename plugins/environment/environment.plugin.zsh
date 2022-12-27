5cefbbae284f6022d24da0d1df80052bf0930353
mattmc3 <mattmc3@gmail.com>
2022-12-26 19:47:58 -0500

    Fix #4

--- plugins/environment/environment.plugin.zsh
+++ plugins/environment/environment.plugin.zsh
@@ -40,6 +40,11 @@ export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-$HOME/.xdg}
 
+for _xdgdir in $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME $XDG_RUNTIME_DIR; do
+  [[ -d "$_xdgdir" ]] || mkdir -p $_xdgdir
+done
+unset _xdgdir
+
 if [[ "$OSTYPE" == darwin* ]]; then
   export XDG_DESKTOP_DIR=${XDG_DESKTOP_DIR:-$HOME/Desktop}
   export XDG_DOCUMENTS_DIR=${XDG_DOCUMENTS_DIR:-$HOME/Documents}
