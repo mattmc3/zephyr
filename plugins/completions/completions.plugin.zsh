480205d74ff0acf762dd6bc06ed7e8a8caeb49cd
mattmc3 <mattmc3@gmail.com>
2022-12-27 22:40:19 -0500

    completions: Remove zsh-utils dep

--- plugins/completions/completions.plugin.zsh
+++ plugins/completions/completions.plugin.zsh
@@ -6,10 +6,43 @@
 # Requirements
 #
 
+zstyle -t ':zephyr:core' initialized || return 1
+
 [[ "$TERM" != 'dumb' ]] || return 1
-0=${(%):-%x}
-: ${ZEPHYR_HOME:=${0:A:h:h:h}}
-zstyle -t ':zephyr:core' initialized || . $ZEPHYR_HOME/lib/init.zsh
+
+if zstyle -T ':zephyr:plugins:completions' use-xdg-basedirs; then
+  # Ensure the cache directory exists.
+  [[ -d "${XDG_CACHE_HOME:=$HOME/.cache}/zsh" ]] || mkdir -p "$XDG_CACHE_HOME/zsh"
+
+  _zcompdump="$XDG_CACHE_HOME/zsh/compdump"
+  _zcompcache="$XDG_CACHE_HOME/zsh/compcache"
+else
+  _zcompdump="${ZDOTDIR:-$HOME}/.zcompdump"
+  _zcompcache="${ZDOTDIR:-$HOME}/.zcompcache"
+fi
+
+#
+# Options
+#
+
+setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
+setopt ALWAYS_TO_END       # Move cursor to the end of a completed word.
+setopt AUTO_MENU           # Show completion menu on a successive tab press.
+setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
+setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
+setopt EXTENDED_GLOB       # Needed for file modification glob modifiers with compinit
+setopt NO_CASE_GLOB        # Case-insensitive globbing.
+setopt NO_MENU_COMPLETE    # Do not autoselect the first completion entry.
+setopt NO_FLOW_CONTROL     # Disable start/stop characters in shell editor.
+
+#
+# Styles
+#
+
+# Use caching to make completion for commands such as dpkg and apt usable.
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path "$_zcompcache"
+zstyle ':completion:*:*:git:*' script $ZEPHYR_HOME/.external/git/git-completion.bash
 
 #
 # Init
@@ -36,8 +69,33 @@ fpath=(
   $fpath
 )
 
-# Use zsh-utils completion.
-source $ZEPHYR_HOME/.external/zsh-utils/completion/completion.plugin.zsh
+# Initialize completion styles. Users can set their preferred completion style by
+# calling `compstyle <compstyle>` in their .zshrc, or by defining their own
+# `compstyle_<name>_setup` functions similar to the zsh prompt system.
+autoload -Uz compstyle
 
-# Set additional compstyles
-zstyle ':completion:*:*:git:*' script $ZPLUGINDIR/.external/git/git-completion.bash
+# Load and initialize the completion system ignoring insecure directories with a
+# cache time of 20 hours, so it should almost always regenerate the first time a
+# shell is opened each day.
+autoload -Uz compinit
+_comp_files=($_zcompdump(Nmh-20))
+if (( $#_comp_files )); then
+  compinit -i -C -d "$_zcompdump"
+else
+  compinit -i -d "$_zcompdump"
+  # Keep $_zcompdump younger than cache time even if it isn't regenerated.
+  touch "$_zcompdump"
+fi
+
+# Compile compdump, if modified, in background to increase startup speed.
+{
+  if [[ -s "$_zcompdump" && (! -s "${_zcompdump}.zwc" || "$_zcompdump" -nt "${_zcompdump}.zwc") ]]; then
+    zcompile "$_zcompdump"
+  fi
+} &!
+
+#
+# Cleanup
+#
+
+unset _cache_dir _comp_files _zcompdump _zcompcache
