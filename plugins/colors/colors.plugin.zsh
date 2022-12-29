85c11a9f89c488ac05d598fbc1e28087126c8b0e
mattmc3 <mattmc3@gmail.com>
2022-12-29 09:51:04 -0500

    color: add deprecation plugin

---
+++ plugins/colors/colors.plugin.zsh
@@ -0,0 +1,5 @@
+echo >&2 "WARNING: Zephyr 'colors' plugin renamed to 'color'."
+echo >&2 "!!!!!!!: This plugin provided with the legacy name is for compatibility only."
+echo >&2 "!!!!!!!: Please update your .zshrc with the new name."
+echo >&2 "!!!!!!!: This deprecated plugin will be removed February 2023."
+source ${0:a:h:h}/color/color.plugin.zsh
