94e81112a8bebde85c5ac40906dcf0b017209e0d
mattmc3 <mattmc3@gmail.com>
2022-12-29 13:18:52 -0500

    Decided on .zephrc, final answer

--- zephyr.zsh
+++ zephyr.zsh
@@ -11,8 +11,8 @@ autoload -U $fpath[1]/*(.:t)
 _zhome=${ZDOTDIR:-${XDG_CONFIG_HOME:=$HOME/.config}/zsh}
 ZSH_CUSTOM="${ZSH_CUSTOM:-$_zhome}"
 
-# load zephyr zstyles if file exists
-[[ -f ${ZDOTDIR:-$HOME}/.zephyr.zsh ]] && . ${ZDOTDIR:-$HOME}/.zephyr.zsh
+# load zstyles from .zephrc if file found
+[[ -f ${ZDOTDIR:-$HOME}/.zephrc ]] && . ${ZDOTDIR:-$HOME}/.zephrc
 
 # get list of plugins from zstyle or plugins variable
 zstyle -a ':zephyr:load' plugins \
