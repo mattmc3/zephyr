f5b091dab0e6b7f7ba853d5a109fba22b6eb27fe
mattmc3 <mattmc3@gmail.com>
2023-01-22 09:53:13 -0500

    Do not assume default plugins anymore

--- zephyr.zsh
+++ zephyr.zsh
@@ -14,28 +14,14 @@ ZSH_CUSTOM="${ZSH_CUSTOM:-$_zhome}"
 # load zstyles from .zstyles if file found
 [[ -f ${ZDOTDIR:-$HOME}/.zstyles ]] && . ${ZDOTDIR:-$HOME}/.zstyles
 
-# get list of plugins from zstyle or plugins variable
-zstyle -a ':zephyr:load' plugins \
-  '_zephyr_plugins' || _zephyr_plugins=($plugins)
-
-# if nothing provided, use the default list
-(( $#_zephyr_plugins )) ||
-  _zephyr_plugins=(
-    color
-    environment
-    editor
-    history
-    directory
-    utility
-    prompt
-    completion
-  )
+# get list of plugins from zstyle
+zstyle -a ':zephyr:load' plugins '_zephyr_plugins'
 
 # Load plugins.
 for _zephyr_plugin in $_zephyr_plugins; do
   _initfiles=(
-    $ZSH_CUSTOM/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh(N)
-    $ZEPHYR_HOME/plugins/$_zephyr_plugin/$_zephyr_plugin.plugin.zsh(N)
+    $ZSH_CUSTOM/plugins/${_zephyr_plugin}/${_zephyr_plugin}.plugin.zsh(N)
+    $ZEPHYR_HOME/plugins/${_zephyr_plugin}/${_zephyr_plugin}.plugin.zsh(N)
   )
   if (( $#_initfiles )); then
     # echo "Loading plugin $_zephyr_plugin from $_initfiles[1]"
