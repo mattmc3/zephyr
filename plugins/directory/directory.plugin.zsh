7dc30affba0be555cdf130540904d2a1e575ca3b
mattmc3 <mattmc3@gmail.com>
2023-01-23 17:28:04 -0500

    Update directory plugin from upstream

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -3,23 +3,20 @@
 ###
 
 #
-# Requirements
-#
-
-[[ "$TERM" != 'dumb' ]] || return 1
-
-#
 # Options
 #
 
-# Directory options
-setopt AUTO_PUSHD            # cd automatically uses the dirstack.
-setopt CDABLE_VARS           # Change directory to a path stored in a variable.
-setopt EXTENDED_GLOB         # Use extended globbing syntax.
-setopt MULTIOS               # Write to multiple descriptors.
-setopt PUSHD_MINUS           # Swap meanings of +/- to be more natural.
-setopt PUSHD_SILENT          # Do not print the dirstack after pushd/popd.
-setopt PUSHD_TO_HOME         # pushd with no args goes to home.
+setopt AUTO_CD              # Auto changes to a directory without typing cd.
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+setopt PUSHD_MINUS          # Swap meanings of +/- to be more natural.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt MULTIOS              # Write to multiple descriptors.
+setopt EXTENDED_GLOB        # Use extended globbing syntax.
+unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
+                            # Use >! and >>! to bypass.
 
 #
 # Aliases
@@ -28,18 +25,13 @@ setopt PUSHD_TO_HOME         # pushd with no args goes to home.
 if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
   alias -- -='cd -'
   alias dirh='dirs -v'
-
-  # set global backref aliases (eg: "..3"="../../..") and
-  # set dirstack aliases (eg: "-4"="cd -4")
-  local _dotdots=".."
-  for _idx ({1..9}); do
-    alias "$_idx"="cd -${_idx}"
-    alias -g "..$_idx"="$_dotdots"
-    _dotdots+='/..'
+  dotdot=".."
+  for index ({1..9}); do
+    alias "$index"="cd +${index}"
+    alias -g "..$index"="$dotdot"
+    dotdot+='/..'
   done
-
-  # clean up
-  unset _idx _dotdots
+  unset index dotdot
 fi
 
 #
