f198f44aea6bcd3daf29a3fb0ee42a4785597829
mattmc3 <mattmc3@gmail.com>
2023-01-23 13:36:10 -0500

    Update clipboard plugin from upstream

--- plugins/clipboard/clipboard.plugin.zsh
+++ plugins/clipboard/clipboard.plugin.zsh
@@ -12,10 +12,21 @@
 # Init
 #
 
-source ${0:A:h:h}/.external/ohmyzsh/lib/clipboard.zsh || return 1
-source ${0:A:h:h}/.external/ohmyzsh/plugins/copybuffer/copybuffer.plugin.zsh || return 1
-source ${0:A:h:h}/.external/ohmyzsh/plugins/copyfile/copyfile.plugin.zsh || return 1
-source ${0:A:h:h}/.external/ohmyzsh/plugins/copypath/copypath.plugin.zsh || return 1
+# Autoload functions.
+fpath=(${0:A:h}/functions $fpath)
+autoload -U $fpath[1]/*(.:t)
+
+# Detect clipboard at load, just like OMZ does.
+detect-clipboard
+
+#
+# Keybindings
+#
+
+zle -N copybuffer
+bindkey -M emacs "^O" copybuffer
+bindkey -M viins "^O" copybuffer
+bindkey -M vicmd "^O" copybuffer
 
 #
 # Cleanup
