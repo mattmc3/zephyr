38a6e88e6f06fbf6f4056fe2c17bf3d87ae45125
mattmc3 <mattmc3@gmail.com>
2023-01-22 21:59:08 -0500

    Refactor z

--- plugins/z/z.plugin.zsh
+++ plugins/z/z.plugin.zsh
@@ -12,11 +12,6 @@
 # Init
 #
 
-source ${0:a:h}/external/zsh-z/zsh-z.plugin.zsh
+source ${0:A:h:h}/.external/zsh-z/init.zsh || return 1
 ZSHZ_DATA=${XDG_DATA_HOME:=$HOME/.local/share}/zsh-z/data
 [[ -f $ZSHZ_DATA ]] || { mkdir -p ${ZSHZ_DATA:h} && touch $ZSHZ_DATA }
-
-# if [[ -z "$_Z_DATA" ]]; then
-#   _Z_DATA=${XDG_DATA_HOME:=$HOME/.local/share}/z/data
-# fi
-# [[ -f $_Z_DATA ]] || { mkdir -p ${_Z_DATA:h} && touch $_Z_DATA }
