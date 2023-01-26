ba7cd9688479657d59427a739a5fa07cca3d8590
mattmc3 <mattmc3@gmail.com>
2023-01-26 17:59:05 -0500

    Autosuggestions name should match prezto module

--- plugins/autosuggestions/autosuggestions.plugin.zsh
+++ plugins/autosuggestions/autosuggestions.plugin.zsh
@@ -1,5 +1,43 @@
-echo >&2 "WARNING: Zephyr 'autosuggestions' plugin renamed to 'autosuggest'."
-echo >&2 "!!!!!!!: This plugin provided with the legacy name is for compatibility only."
-echo >&2 "!!!!!!!: Please update your .zshrc with the new name."
-echo >&2 "!!!!!!!: This deprecated plugin will be removed March 2023."
-source ${0:a:h:h}/autosuggest/autosuggest.plugin.zsh
+####
+# autosuggestions - Fish-like autosuggestions for zsh.
+###
+
+#
+# Requirements
+#
+
+[[ "$TERM" != 'dumb' ]] || return 1
+
+if ! zstyle -t ':zephyr:core' initialized; then
+  source ${0:A:h:h}/zephyr/zephyr.plugin.zsh
+fi
+
+#
+# Init
+#
+
+source ${0:A:h:h}/.external/zsh-autosuggestions/init.zsh || return 1
+
+#
+# Variables
+#
+
+# Set highlight color, default 'fg=60'.
+zstyle -s ':zephyr:plugin:autosuggestions:highlight' style \
+  'ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE' || ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
+
+#
+# Keybinds
+#
+
+if [[ -n "$key_info" ]]; then
+  # vi
+  bindkey -M viins "$key_info[Control]F" vi-forward-word
+  bindkey -M viins "$key_info[Control]E" vi-add-eol
+fi
+
+#
+# Wrap up
+#
+
+zstyle ":zephyr:plugin:autosuggestions" loaded 'yes'
