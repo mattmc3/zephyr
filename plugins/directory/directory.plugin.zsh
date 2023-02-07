560825de87cbc74b570a9dbcdac76ff037658784
mattmc3 <mattmc3@gmail.com>
2023-02-07 11:17:15 -0500

    Update directory plugin

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -6,17 +6,15 @@
 # Options
 #
 
-setopt AUTO_CD              # Auto changes to a directory without typing cd.
-setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
-setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
-setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
-setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
-setopt PUSHD_MINUS          # Swap meanings of +/- to be more natural.
-setopt CDABLE_VARS          # Change directory to a path stored in a variable.
-setopt MULTIOS              # Write to multiple descriptors.
-setopt EXTENDED_GLOB        # Use extended globbing syntax.
-unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
-                            # Use >! and >>! to bypass.
+setopt AUTO_PUSHD                # Make cd push the old dir onto the dirstack.
+setopt PUSHD_IGNORE_DUPS         # Don’t push multiple copies of a dir to the dirstack.
+setopt PUSHD_SILENT              # Do not print the dirstack after pushd or popd.
+setopt PUSHD_MINUS               # Exchange meanings of +/- when navigating the dirstack.
+setopt PUSHD_TO_HOME             # Push to home directory when no argument is given.
+setopt CDABLE_VARS               # Change directory to a path stored in a variable.
+setopt MULTIOS                   # Write to multiple descriptors.
+setopt EXTENDED_GLOB             # Use extended globbing syntax.
+setopt NO_CLOBBER                # Do not overwrite files with >. Use >| to bypass.
 
 #
 # Aliases
