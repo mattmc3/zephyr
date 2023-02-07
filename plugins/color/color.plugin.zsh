556b0914efc5dfdb0f01d370425e1aff86de7fba
mattmc3 <mattmc3@gmail.com>
2023-02-07 08:28:37 -0500

    Update color plugin

--- plugins/color/color.plugin.zsh
+++ plugins/color/color.plugin.zsh
@@ -9,6 +9,20 @@
 [[ "$TERM" != 'dumb' ]] || return 1
 
 #
+# Variables
+#
+
+# start/end - md/me:bold; us/ue:underline; so/se:standout;
+# colors    - 0-black; 1-red; 2-green; 3-yellow; 4-blue; 5-magenta; 6-cyan; 7-white;
+export LESS_TERMCAP_mb=$'\e[01;36m'
+export LESS_TERMCAP_md=$'\e[01;36m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[00;47;30m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[01;35m'
+
+#
 # Init
 #
 
