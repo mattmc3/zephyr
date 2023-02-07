e12929bf6f55e8320da76d2deda20b4864ac95a8
mattmc3 <mattmc3@gmail.com>
2023-02-07 13:09:53 -0500

    Update history

--- plugins/history/history.plugin.zsh
+++ plugins/history/history.plugin.zsh
@@ -2,13 +2,15 @@
 # history - Set Zsh history options.
 ###
 
+
 #
 # Options
 #
 
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt SHARE_HISTORY             # Share history between all sessions.
+setopt NO_SHARE_HISTORY          # Don't share history between all sessions.
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
@@ -16,8 +18,9 @@ setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
 setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt HIST_REDUCE_BLANKS        # Remove extra blanks from commands added to the history list.
 setopt NO_HIST_BEEP              # Do not beep when accessing non-existent history.
+
 
 #
 # Variables
@@ -33,7 +36,8 @@ SAVEHIST="${_savehist}"  # max history events saved to history file
 unset _hist{file,size} _savehist
 
 # Make sure the path to the history file exists
-[[ -f $HISTFILE ]] || { mkdir -p ${HISTFILE:h} && touch $HISTFILE }
+[[ -f $HISTFILE ]] || { mkdir -p $HISTFILE:h && touch $HISTFILE }
+
 
 #
 # Aliases
@@ -44,6 +48,7 @@ if ! zstyle -t ':zephyr:plugin:history:alias' skip; then
   alias history-stat="command history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
   alias hist='fc -li'
 fi
+
 
 #
 # Wrap up
