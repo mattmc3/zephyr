7b66e419b2a674e58fd37803c8517c04522c06c1
mattmc3 <mattmc3@gmail.com>
2023-04-07 17:33:11 -0400

    Update conf.d plugin

--- plugins/confd/confd.plugin.zsh
+++ plugins/confd/confd.plugin.zsh
@@ -1,11 +1,6 @@
-###
-# conf.d - Use a Fish-like conf.d directory for sourcing configs.
-###
+##? conf.d - Use a Fish-like conf.d directory for sourcing configs.
 
-#
-# Init
-#
-
+# Try to detect conf.d.
 _zhome=${ZDOTDIR:-${XDG_CONFIG_HOME:=HOME/.config}/zsh}
 _confd=(
   $_zhome/zshrc.d(N)
@@ -14,16 +9,14 @@ _confd=(
   ${ZDOTDIR:-$HOME}/.zshrc.d(N)
 )
 (( $#_confd )) || return 1
+
+# Source all files in conf.d.
 for _rcfile in $_confd[1]/*.{z,}sh(N); do
   # ignore files that begin with ~
   case ${_rcfile:t} in '~'*) continue;; esac
   source "$_rcfile"
 done
-
-#
-# Wrap up
-#
-
 unset _rcfile _confd _zhome
 
+# Tell Zephyr this plugin is loaded.
 zstyle ":zephyr:plugin:confd" loaded 'yes'
