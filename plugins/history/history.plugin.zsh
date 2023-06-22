90627fff66eafa6097ff945ccc761398d4b692e6
mattmc3 <mattmc3@gmail.com>
2023-06-22 12:27:38 -0400

    Refactor

--- plugins/history/history.plugin.zsh
+++ plugins/history/history.plugin.zsh
@@ -2,56 +2,58 @@
 # history - Set history options and define history aliases.
 #
 
-() {
-  local histopts=(
-    # 16.2.4 History
-    bang_hist               # Treat the '!' character specially during expansion.
-    extended_history        # Write the history file in the ':start:elapsed;command' format.
-    hist_expire_dups_first  # Expire a duplicate event first when trimming history.
-    hist_find_no_dups       # Do not display a previously found event.
-    hist_ignore_all_dups    # Delete an old recorded event if a new event is a duplicate.
-    hist_ignore_dups        # Do not record an event that was just recorded again.
-    hist_ignore_space       # Do not record an event starting with a space.
-    hist_reduce_blanks      # Remove extra blanks from commands added to the history list.
-    hist_save_no_dups       # Do not write a duplicate event to the history file.
-    hist_verify             # Do not execute immediately upon history expansion.
-    inc_append_history      # Write to the history file immediately, not when the shell exits.
-    NO_hist_beep            # Don't beep when accessing non-existent history.
-    NO_share_history        # Don't share history between all sessions.
-  )
-  setopt $histopts
+#
+# References
+#
 
-  # The path to the history file.
-  zstyle -s ':zephyr:plugin:history' histfile 'HISTFILE' ||
-    HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history"
-  [[ -d $HISTFILE:h ]] || mkdir -p $HISTFILE:h
+# https://github.com/sorin-ionescu/prezto/tree/master/modules/history
+# https://zsh.sourceforge.io/Doc/Release/Options.html#History
 
-  # remove in the future
-  if [[ -f ${XDG_DATA_HOME:-$HOME/.local/share}/zsh/history ]] &&
-     [[ ! -f ${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history ]]
-  then
-    cp ${XDG_DATA_HOME:-$HOME/.local/share}/zsh/history ${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history
-  fi
+#
+# Options
+#
 
-  # Session history items, and history file items
-  zstyle -s ':zephyr:plugin:history' histsize 'HISTSIZE' ||
-    HISTSIZE=10000
-  zstyle -s ':zephyr:plugin:history' savehist 'SAVEHIST' ||
-    SAVEHIST=10000
+setopt bang_hist               # Treat the '!' character specially during expansion.
+setopt extended_history        # Write the history file in the ':start:elapsed;command' format.
+setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
+setopt hist_find_no_dups       # Do not display a previously found event.
+setopt hist_ignore_all_dups    # Delete an old recorded event if a new event is a duplicate.
+setopt hist_ignore_dups        # Do not record an event that was just recorded again.
+setopt hist_ignore_space       # Do not record an event starting with a space.
+setopt hist_reduce_blanks      # Remove extra blanks from commands added to the history list.
+setopt hist_save_no_dups       # Do not write a duplicate event to the history file.
+setopt hist_verify             # Do not execute immediately upon history expansion.
+setopt inc_append_history      # Write to the history file immediately, not when the shell exits.
+setopt NO_hist_beep            # Don't beep when accessing non-existent history.
+setopt NO_share_history        # Don't share history between all sessions.
 
-  #
-  # Aliases
-  #
+#
+# Variables
+#
 
-  if ! zstyle -t ':zephyr:history:alias' skip; then
-    alias hist='fc -li'
-    alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
-  fi
+# The path to the history file.
+zstyle -s ':zephyr:plugin:history' histfile 'HISTFILE' ||
+  HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history"
+[[ -d $HISTFILE:h ]] || mkdir -p $HISTFILE:h
 
-  #
-  # Wrap up
-  #
+# Session history items (default 2000), and history file items (default 1000)
+zstyle -s ':zephyr:plugin:history' histsize 'HISTSIZE' ||
+  HISTSIZE=10000
+zstyle -s ':zephyr:plugin:history' savehist 'SAVEHIST' ||
+  SAVEHIST=10000
 
-  # Tell Zephyr this plugin is loaded.
-  zstyle ':zephyr:plugin:history' loaded 'yes'
-}
+#
+# Aliases
+#
+
+if ! zstyle -t ':zephyr:plugin:history:alias' skip; then
+  alias hist='fc -li'
+  alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+fi
+
+#
+# Wrap up
+#
+
+# Tell Zephyr this plugin is loaded.
+zstyle ':zephyr:plugin:history' loaded 'yes'
