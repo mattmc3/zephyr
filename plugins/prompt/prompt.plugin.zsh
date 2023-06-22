cc5cf573ce20967e3295d7659352af29f996f629
mattmc3 <mattmc3@gmail.com>
2023-06-22 15:38:43 -0400

    Fix if statement

--- plugins/prompt/prompt.plugin.zsh
+++ plugins/prompt/prompt.plugin.zsh
@@ -30,7 +30,7 @@ local -a prompt_theme
 zstyle -a ':zephyr:plugin:prompt' theme 'prompt_argv'
 if [[ $TERM == (dumb|linux|*bsd*) ]]; then
   prompt 'off'
-elif (( $#prompt_argv > 0 ))
+elif (( $#prompt_argv > 0 )); then
   prompt "$prompt_argv[@]"
 fi
 unset prompt_argv
