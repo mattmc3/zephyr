664f8020278a7581e50e0d895b964be5d009fe94
mattmc3 <mattmc3@gmail.com>
2023-06-22 10:58:24 -0400

    Rework terminal pt3

---
+++ plugins/terminal/terminal.plugin.zsh
@@ -0,0 +1,26 @@
+#
+# terminal - Sets terminal window and tab titles.
+#
+
+#
+# Requirements
+#
+
+# Return if requirements are not found.
+if [[ "$TERM" == (dumb|linux|*bsd*|eterm*) ]]; then
+  return 1
+fi
+
+#
+# Init
+#
+
+# Use Prezto's terminal module.
+source ${0:A:h}/external/prezto_terminal.zsh
+
+#
+# Wrap up
+#
+
+# Tell Zephyr this plugin is loaded.
+zstyle ':zephyr:plugin:terminal' loaded 'yes'
