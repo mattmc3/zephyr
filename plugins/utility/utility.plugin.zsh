63ed9ae5e64fa54bfe8a6cca9c27ececa0cc2d16
mattmc3 <mattmc3@gmail.com>
2023-06-22 10:37:57 -0400

    Code cleanup

--- plugins/utility/utility.plugin.zsh
+++ plugins/utility/utility.plugin.zsh
@@ -7,32 +7,33 @@
 #
 
 # Glob options.
-setopt EXTENDED_GLOB         # Use more awesome globbing features.
-setopt GLOB_DOTS             # Include dotfiles when globbing.
-setopt NO_RM_STAR_SILENT     # Ask for confirmation for `rm *' or `rm path/*'
+setopt extended_glob         # Use more awesome globbing features.
+setopt glob_dots             # Include dotfiles when globbing.
+setopt no_rm_star_silent     # Ask for confirmation for `rm *' or `rm path/*'
 
 # General options.
-setopt COMBINING_CHARS       # Combine 0-len chars with the base character (eg: accents).
-setopt INTERACTIVE_COMMENTS  # Enable comments in interactive shell.
-setopt RC_QUOTES             # Allow 'Hitchhikers''s Guide' instead of 'Hitchhikers'\''s Guide'.
-setopt NO_MAIL_WARNING       # Don't print a warning message if a mail file has been accessed.
-setopt NO_BEEP               # Don't Beep on error in line editor.
+setopt combining_chars       # Combine 0-len chars with the base character (eg: accents).
+setopt interactive_comments  # Enable comments in interactive shell.
+setopt rc_quotes             # Allow 'Hitchhikers''s Guide' instead of 'Hitchhikers'\''s Guide'.
+setopt NO_mail_warning       # Don't print a warning message if a mail file has been accessed.
+setopt NO_beep               # Don't Beep on error in line editor.
 
 # Job options.
-setopt LONG_LIST_JOBS        # List jobs in the long format by default.
-setopt AUTO_RESUME           # Attempt to resume existing job before creating a new process.
-setopt NOTIFY                # Report status of background jobs immediately.
-setopt NO_BG_NICE            # Don't run all background jobs at a lower priority.
-setopt NO_HUP                # Don't kill jobs on shell exit.
-setopt NO_CHECK_JOBS         # Don't report on jobs when shell exit.
+setopt long_list_jobs        # List jobs in the long format by default.
+setopt auto_resume           # Attempt to resume existing job before creating a new process.
+setopt notify                # Report status of background jobs immediately.
+setopt NO_bg_nice            # Don't run all background jobs at a lower priority.
+setopt NO_hup                # Don't kill jobs on shell exit.
+setopt NO_check_jobs         # Don't report on jobs when shell exit.
 
 #
 # Functions
 #
 
-# Autoload functions.
+# Load plugin functions.
+0=${(%):-%N}
 fpath=(${0:A:h}/functions $fpath)
-autoload -Uz $fpath[1]/*(.:t)
+autoload -Uz ${0:A:h}/functions/*(.:t)
 
 # Use built-in paste magic.
 autoload -Uz bracketed-paste-url-magic
@@ -46,12 +47,12 @@ zle -N self-insert url-quote-magic
 
 # Common utils everywhere.
 
-# envsubst
+# Linux-like envsubst command
 if ! (( $+commands[envsubst] )); then
   alias envsubst="python -c 'import os,sys;[sys.stdout.write(os.path.expandvars(l)) for l in sys.stdin]'"
 fi
 
-# open
+# macOS-like open command
 if ! (( $+commands[open] )); then
   if [[ "$OSTYPE" == cygwin* ]]; then
     alias open='cygstart'
@@ -62,7 +63,7 @@ if ! (( $+commands[open] )); then
   fi
 fi
 
-# pbcopy/pbpaste
+# macOS-like pbcopy/pbpaste command
 if ! (( $+commands[pbcopy] )); then
   if [[ "$OSTYPE" == cygwin* ]]; then
     alias pbcopy='tee > /dev/clipboard'
