6440cb687309ce86f1d1e9cf6029b3bc24b849c8
mattmc3 <mattmc3@gmail.com>
2023-12-20 15:15:39 -0500

    directory: Cleanup

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -2,41 +2,21 @@
 # directory - Set directory options and define directory aliases.
 #
 
-#
-# Requirements
-#
-
 # Return if requirements are not found.
 [[ "$TERM" != 'dumb' ]] || return 1
-0=${(%):-%N}
 
-#
-# Options
-#
+# Set Zsh options related to directories, globbing, and I/O.
+setopt auto_pushd         # Make cd push the old directory onto the dirstack.
+setopt pushd_ignore_dups  # Don’t push multiple copies of the same directory onto the dirstack.
+setopt pushd_minus        # Exchanges meanings of +/- when navigating the dirstack.
+setopt pushd_silent       # Do not print the directory stack after pushd or popd.
+setopt pushd_to_home      # Push to home directory when no argument is given.
+setopt extended_glob      # Use extended globbing syntax.
+setopt glob_dots          # Don't hide dotfiles from glob patterns.
+setopt multios            # Write to multiple descriptors.
+setopt NO_clobber         # Don't overwrite files with >. Use >| to bypass.
 
-# 16.2.1 Changing Directories
-setopt auto_cd                 # If a command isn't valid, but is a directory, cd to that dir.
-setopt auto_pushd              # Make cd push the old directory onto the dirstack.
-setopt cdable_vars             # Change directory to a path stored in a variable.
-setopt pushd_ignore_dups       # Don’t push multiple copies of the same directory onto the dirstack.
-setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
-setopt pushd_silent            # Do not print the directory stack after pushd or popd.
-setopt pushd_to_home           # Push to home directory when no argument is given.
-
-# 16.2.3 Expansion and Globbing
-setopt extended_glob           # Use extended globbing syntax.
-setopt glob_dots               # Don't hide dotfiles from glob patterns.
-
-# 16.2.6 Input/Output
-setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
-
-# 16.2.9 Scripts and Functions
-setopt multios                 # Write to multiple descriptors.
-
-#
-# Aliases
-#
-
+# Set aliases.
 if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
   # directory aliases
   alias -- -='cd -'
@@ -50,10 +30,6 @@ if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
   done
   unset index
 fi
-
-#
-# Wrap up
-#
 
 # Tell Zephyr this plugin is loaded.
 zstyle ':zephyr:plugin:directory' loaded 'yes'
