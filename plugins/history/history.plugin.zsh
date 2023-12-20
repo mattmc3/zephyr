0b8dd4c23387f8c96b3e96653b893db80b48dfad
mattmc3 <mattmc3@gmail.com>
2023-12-20 15:20:44 -0500

    history: Cleanup

--- plugins/history/history.plugin.zsh
+++ plugins/history/history.plugin.zsh
@@ -2,17 +2,11 @@
 # history - Set history options and define history aliases.
 #
 
-#
-# References
-#
+# References:
+# - https://github.com/sorin-ionescu/prezto/tree/master/modules/history
+# - https://zsh.sourceforge.io/Doc/Release/Options.html#History
 
-# https://github.com/sorin-ionescu/prezto/tree/master/modules/history
-# https://zsh.sourceforge.io/Doc/Release/Options.html#History
-
-#
-# Options
-#
-
+# Set Zsh options related to history.
 setopt bang_hist               # Treat the '!' character specially during expansion.
 setopt extended_history        # Write the history file in the ':start:elapsed;command' format.
 setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
@@ -27,33 +21,24 @@ setopt inc_append_history      # Write to the history file immediately, not when
 setopt NO_hist_beep            # Don't beep when accessing non-existent history.
 setopt NO_share_history        # Don't share history between all sessions.
 
-#
-# Variables
-#
-
-# The path to the history file.
+# Set the path to the history file.
 zstyle -s ':zephyr:plugin:history' histfile 'HISTFILE' ||
   HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history"
 [[ -d $HISTFILE:h ]] || mkdir -p $HISTFILE:h
 
-# Session history items (default 2000), and history file items (default 1000)
-zstyle -s ':zephyr:plugin:history' histsize 'HISTSIZE' ||
-  HISTSIZE=10000
+# Set history file size (default 1000).
 zstyle -s ':zephyr:plugin:history' savehist 'SAVEHIST' ||
   SAVEHIST=10000
 
-#
-# Aliases
-#
+# Set session history size (default: 2000).
+zstyle -s ':zephyr:plugin:history' histsize 'HISTSIZE' ||
+  HISTSIZE=20000
 
+# Set Zsh aliases related to history.
 if ! zstyle -t ':zephyr:plugin:history:alias' skip; then
   alias hist='fc -li'
   alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
 fi
-
-#
-# Wrap up
-#
 
 # Tell Zephyr this plugin is loaded.
 zstyle ':zephyr:plugin:history' loaded 'yes'
