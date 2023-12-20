36cf772707aaacfc671711a0809aae4ca19aace9
mattmc3 <mattmc3@gmail.com>
2023-12-20 15:16:23 -0500

    confd: Cleanup

--- plugins/confd/confd.plugin.zsh
+++ plugins/confd/confd.plugin.zsh
@@ -2,12 +2,11 @@
 # conf.d - Use a Fish-like conf.d directory for sourcing configs.
 #
 
-#
-# Variables
-#
+# Bootstrap.
+0=${(%):-%N}
+zstyle -t ':zephyr:lib:boostrap' loaded || source ${0:a:h:h:h}/lib/boostrap.zsh
 
-: ${__zsh_config_dir:=${ZDOTDIR:-${XDG_CONFIG_HOME:-$HOME/.config}/zsh}}
-
+# Find the conf.d directory.
 zstyle -a ':zephyr:plugin:confd' directory '_confd' ||
   _confd=(
     $__zsh_config_dir/conf.d(N)
@@ -17,26 +16,15 @@ zstyle -a ':zephyr:plugin:confd' directory '_confd' ||
   )
 (( $#_confd )) || return 1
 
-#
-# Init
-#
-
-# Source all files in conf.d.
+# Source all scripts in conf.d.
 for _rcfile in $_confd[1]/*.{z,}sh(N); do
   # ignore files that begin with ~
   [[ ${_rcfile:t} == '~'* ]] && continue
   source $_rcfile
 done
 
-#
-# Clean up
-#
-
+# Clean up.
 unset _rcfile _confd
-
-#
-# Wrap up
-#
 
 # Tell Zephyr this plugin is loaded.
 zstyle ':zephyr:plugin:confd' loaded 'yes'
