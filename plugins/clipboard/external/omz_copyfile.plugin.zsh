221b6e78de679b6254018bc9a9d44f06b7d9c04c
mattmc3 <mattmc3@gmail.com>
2023-12-21 10:34:05 -0500

    clipboard: Remove unnecessary plugin

--- plugins/clipboard/external/omz_copyfile.plugin.zsh
+++
@@ -1,9 +0,0 @@
-# Generated from https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/copyfile/copyfile.plugin.zsh
-#
-# Copies the contents of a given file to the system or X Windows clipboard
-#
-# copyfile <file>
-function copyfile {
-  emulate -L zsh
-  clipcopy $1
-}
