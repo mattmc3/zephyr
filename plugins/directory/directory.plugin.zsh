0d8269679b139ad524fdc5a22f3c85d5fc3c1e03
mattmc3 <mattmc3@gmail.com>
2024-01-12 09:29:40 -0500

    Update directory plugin

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -9,15 +9,16 @@
 [[ "$TERM" != 'dumb' ]] || return 1
 
 # Set Zsh options related to directories, globbing, and I/O.
-setopt auto_pushd         # Make cd push the old directory onto the dirstack.
-setopt pushd_ignore_dups  # Don’t push multiple copies of the same directory onto the dirstack.
-setopt pushd_minus        # Exchanges meanings of +/- when navigating the dirstack.
-setopt pushd_silent       # Do not print the directory stack after pushd or popd.
-setopt pushd_to_home      # Push to home directory when no argument is given.
-setopt extended_glob      # Use extended globbing syntax.
-setopt glob_dots          # Don't hide dotfiles from glob patterns.
-setopt multios            # Write to multiple descriptors.
-setopt NO_clobber         # Don't overwrite files with >. Use >| to bypass.
+setopt auto_pushd           # Make cd push the old directory onto the dirstack.
+setopt pushd_ignore_dups    # Don’t push multiple copies of the same directory onto the dirstack.
+setopt pushd_minus          # Exchanges meanings of +/- when navigating the dirstack.
+setopt pushd_silent         # Do not print the directory stack after pushd or popd.
+setopt pushd_to_home        # Push to home directory when no argument is given.
+setopt multios              # Write to multiple descriptors.
+setopt extended_glob        # Use extended globbing syntax.
+setopt glob_dots            # Don't hide dotfiles from glob patterns.
+setopt NO_clobber           # Don't overwrite files with >. Use >| to bypass.
+setopt NO_rm_star_silent    # Ask for confirmation for `rm *' or `rm path/*'
 
 # Set aliases.
 if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
@@ -25,14 +26,18 @@ if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
   alias -- -='cd -'
   alias dirh='dirs -v'
 
-  for index in {1..9}; do
-    # dirstack aliases (eg: "2"="cd 2")
-    alias "$index"="cd +${index}"
-    # backref aliases (eg: "..3"="../../..")
-    alias -g "..$index"=$(printf '../%.0s' {1..$index})
-  done
-  unset index
+  () {
+    local i
+    local -a dotdot=('..')
+    for i in {1..9}; do
+      # dirstack aliases (eg: "2"="cd 2")
+      alias "$i"="cd -${i}"
+      # backref aliases (eg: "..3"="../../..")
+      alias -g "..$i"=${(pj;/;)dotdot}
+      dotdot+=('..')
+    done
+  }
 fi
 
-# Tell Zephyr this plugin is loaded.
+# Mark this plugin as loaded.
 zstyle ':zephyr:plugin:directory' loaded 'yes'
