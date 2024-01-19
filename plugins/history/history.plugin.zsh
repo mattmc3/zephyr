039a89e4e514ab36b8ea8aab13ab9606c1658de8
mattmc3 <mattmc3@gmail.com>
2024-01-19 12:01:39 -0500

    history: Support non-XDG setups

--- plugins/history/history.plugin.zsh
+++ plugins/history/history.plugin.zsh
@@ -6,7 +6,8 @@
 # - https://github.com/sorin-ionescu/prezto/tree/master/modules/history
 # - https://zsh.sourceforge.io/Doc/Release/Options.html#History
 
-# Set Zsh options related to history.
+# Set Zsh history options.
+# 16.2.4: https://zsh.sourceforge.io/Doc/Release/Options.html#History
 setopt bang_hist               # Treat the '!' character specially during expansion.
 setopt extended_history        # Write the history file in the ':start:elapsed;command' format.
 setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
@@ -22,13 +23,21 @@ setopt NO_hist_beep            # Don't beep when accessing non-existent history.
 setopt NO_share_history        # Don't share history between all sessions.
 
 # Set the path to the history file.
-zstyle -s ':zephyr:plugin:history' histfile 'HISTFILE' ||
-  HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history"
-[[ -d $HISTFILE:h ]] || mkdir -p $HISTFILE:h
+zstyle -s ':zephyr:plugin:history' histfile '_zhistfile'
+if [[ -z "$_zhistfile" ]]; then
+  if zstyle -T ':zephyr:plugin:history' use-xdg-basedirs; then
+    _zhistfile=${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history
+  else
+    _zhistfile=${ZDOTDIR:-$HOME}/.zsh_history
+  fi
+fi
+[[ -d $_zhistfile:h ]] || mkdir -p $_zhistfile:h
+HISTFILE=$_zhistfile
+unset _zhistfile
 
 # Set history file size (default 1000).
 zstyle -s ':zephyr:plugin:history' savehist 'SAVEHIST' ||
-  SAVEHIST=10000
+  SAVEHIST=100000
 
 # Set session history size (default: 2000).
 zstyle -s ':zephyr:plugin:history' histsize 'HISTSIZE' ||
