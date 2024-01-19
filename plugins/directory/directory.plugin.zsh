c7646b6dc00552c81fc797fc9edd6d6e2113b7a5
mattmc3 <mattmc3@gmail.com>
2024-01-19 12:09:27 -0500

    directory: Remove ignore dupe opt and cleanup

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -9,31 +9,26 @@
 [[ "$TERM" != 'dumb' ]] || return 1
 
 # Set Zsh options related to directories, globbing, and I/O.
-setopt auto_pushd           # Make cd push the old directory onto the dirstack.
-setopt pushd_ignore_dups    # Don’t push multiple copies of the same directory onto the dirstack.
-setopt pushd_minus          # Exchanges meanings of +/- when navigating the dirstack.
-setopt pushd_silent         # Do not print the directory stack after pushd or popd.
-setopt pushd_to_home        # Push to home directory when no argument is given.
-setopt multios              # Write to multiple descriptors.
-setopt extended_glob        # Use extended globbing syntax.
-setopt glob_dots            # Don't hide dotfiles from glob patterns.
-setopt NO_clobber           # Don't overwrite files with >. Use >| to bypass.
-setopt NO_rm_star_silent    # Ask for confirmation for `rm *' or `rm path/*'
+# 16.2.1 https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories
+setopt auto_pushd              # Make cd push the old directory onto the dirstack.
+setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
+setopt pushd_silent            # Do not print the directory stack after pushd or popd.
+setopt pushd_to_home           # Push to home directory when no argument is given.
+setopt multios                 # Write to multiple descriptors.
+setopt extended_glob           # Use extended globbing syntax.
+setopt glob_dots               # Don't hide dotfiles from glob patterns.
+setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
+setopt NO_rm_star_silent       # Ask for confirmation for `rm *' or `rm path/*'
 
-# Set aliases.
+# Set directory aliases.
 if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
-  # directory aliases
   alias -- -='cd -'
   alias dirh='dirs -v'
-
   () {
-    local i
-    local -a dotdot=('..')
+    local i dotdot=('..')
     for i in {1..9}; do
-      # dirstack aliases (eg: "2"="cd 2")
-      alias "$i"="cd -${i}"
-      # backref aliases (eg: "..3"="../../..")
-      alias -g "..$i"=${(pj;/;)dotdot}
+      alias "$i"="cd -${i}"             # dirstack aliases (eg: "3"="cd -3")
+      alias -g "..$i"=${(pj:/:)dotdot}  # backref aliases (eg: "..3"="../../..")
       dotdot+=('..')
     done
   }
