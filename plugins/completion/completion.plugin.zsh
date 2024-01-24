db83a35f71ab20cadcd745feff86c1cac22ac1dc
mattmc3 <mattmc3@gmail.com>
2024-01-24 13:52:17 -0500

    completion: Enhance completion logic
    - Attach compinit to the first call of precmd
    - Queue compdef calls

--- plugins/completion/completion.plugin.zsh
+++ plugins/completion/completion.plugin.zsh
@@ -51,15 +51,59 @@ fi
 # Add custom completions.
 fpath=(${ZDOTDIR:-${XDG_CONFIG_HOME:-$HOME/.config}/zsh}/completions(-/FN) $fpath)
 
-# Initialize completions.
-if ! zstyle -t ':zephyr:plugin:completion' manual; then
+# Let's talk compinit... compinit works by finding _completion files in your fpath. That
+# means fpath has to be fully populated prior to calling compinit. If you use oh-my-zsh,
+# if populates fpath and runs compinit prior to loading plugins. This is only
+# problematic if you expect to be able to add to fpath later. Conversely, if you wait to
+# run compinit until the end of your config, then functions like compdef aren't
+# available earlier in your config.
+#
+# TLDR; this code handles all those use cases and simply queues calls to compdef and
+# hooks compinit to precmd for one call only, which happens automatically at the end of
+# your config. You can override this behavior with zstyles.
+if zstyle -t ':zephyr:plugin:completion' immediate; then
   run-compinit
+else
+  # Define compinit placeholder functions (compdef) so we can queue up calls to compdef.
+  # That way when the real compinit is called, we can execute the queue.
+  typeset -gHa __zephyr_compdef_queue=()
+  function compdef {
+    (( $# )) || return
+    local compdef_args=("${@[@]}")
+    __zephyr_compdef_queue+=("$(typeset -p compdef_args)")
+  }
+
+  # Wrap compinit temporarily so that when the real compinit call happens, the
+  # queue of compdef calls is processed.
+  function compinit {
+    unfunction compinit compdef &>/dev/null
+    autoload -Uz compinit && compinit "$@"
+    local typedef_compdef_args
+    for typedef_compdef_args in $__zephyr_compdef_queue; do
+      eval $typedef_compdef_args
+      compdef "$compdef_args[@]"
+    done
+    unset __zephyr_compdef_queue
+  }
+
+  # If the user didn't specify they wanted to manually initialize completions,
+  # then attach compinit to the precmd hook so that it happens at the very end.
+  if ! zstyle -t ':zephyr:plugin:completion' manual; then
+    autoload -Uz add-zsh-hook
+    function run-compinit-precmd {
+      run-compinit
+      add-zsh-hook -d precmd run-compinit-precmd
+    }
+    add-zsh-hook precmd run-compinit-precmd
+  fi
 fi
 
 # Set the completion style
 zstyle -s ':zephyr:plugin:completion' compstyle 'zcompstyle' || zcompstyle=zephyr
 if (( $+functions[compstyle_${zcompstyle}_setup] )); then
   compstyle_${zcompstyle}_setup
+else
+  compstyleinit && compstyle ${zcompstyle}
 fi
 unset zcompstyle
 
