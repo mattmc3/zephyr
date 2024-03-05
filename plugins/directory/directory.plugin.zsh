cff313902fad171cf70ee5c454c61fd1061e6cfb
mattmc3 <mattmc3@gmail.com>
2024-03-05 15:57:42 -0500

    directory: Cleanup

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -4,21 +4,27 @@
 
 # References:
 # - https://github.com/sorin-ionescu/prezto/tree/master/modules/directory
+# - https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories
 
 # Return if requirements are not found.
 [[ "$TERM" != 'dumb' ]] || return 1
 
-# Set Zsh options related to directories, globbing, and I/O.
-# 16.2.1 https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories
+# 16.2.3 Expansion and Globbing
+setopt extended_glob           # Use more awesome globbing features.
+setopt glob_dots               # Include dotfiles when globbing.
+
+# 16.2.1 Changing Directories
 setopt auto_pushd              # Make cd push the old directory onto the dirstack.
 setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
 setopt pushd_silent            # Do not print the directory stack after pushd or popd.
 setopt pushd_to_home           # Push to home directory when no argument is given.
-setopt multios                 # Write to multiple descriptors.
-setopt extended_glob           # Use extended globbing syntax.
-setopt glob_dots               # Don't hide dotfiles from glob patterns.
+
+# 16.2.6 Input/Output
 setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
 setopt NO_rm_star_silent       # Ask for confirmation for `rm *' or `rm path/*'
+
+# 16.2.9 Scripts and Functions
+setopt multios                 # Write to multiple descriptors.
 
 # Set directory aliases.
 if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
@@ -36,7 +42,7 @@ fi
 
 if ! zstyle -t ':zephyr:plugin:directory:function' skip; then
   ##? up: Quickly go up any number of directories.
-  up() {
+  function up {
     local parents=${1:-1}
     if ! [[ "$parents" -gt 0 ]]; then
       echo >&2 "up: expecting numeric argument: number of parent dirs"
