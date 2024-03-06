3a3b29eba229dc52a9d8831e2cd7b41d0531314b
mattmc3 <mattmc3@gmail.com>
2024-03-06 12:21:53 -0500

    color: Refactor

--- plugins/color/color.plugin.zsh
+++ plugins/color/color.plugin.zsh
@@ -1,5 +1,5 @@
 #
-# color - Make terminal things more colorful
+# Color: Make terminal things more colorful
 #
 
 # Return if requirements are not found.
@@ -14,33 +14,35 @@ if ! zstyle -t ':zephyr:plugin:color:function' skip; then
   autoload-dir ${0:a:h}/functions
 fi
 
+#region zephyr_plugin_color
+# Built-in zsh colors.
+autoload -Uz colors && colors
+
 # Colorize man pages.
-# mb/me := start/end blink mode      md/me := start/end bold mode
-# so/se := start/end standout mode   us/ue := start/end underline mode
-# 0-black, 1-red, 2-green, 3-yellow, 4-blue, 5-magenta, 6-cyan, 7-white
-export LESS_TERMCAP_mb=$'\e[01;36m'
-export LESS_TERMCAP_md=$'\e[01;36m'
-export LESS_TERMCAP_me=$'\e[0m'
-export LESS_TERMCAP_se=$'\e[0m'
-export LESS_TERMCAP_so=$'\e[00;47;30m'
-export LESS_TERMCAP_ue=$'\e[0m'
-export LESS_TERMCAP_us=$'\e[04;35m'
+export LESS_TERMCAP_md=$fg_bold[blue]   # start bold
+export LESS_TERMCAP_mb=$fg_bold[blue]   # start blink
+export LESS_TERMCAP_so=$'\e[00;47;30m'  # start standout: white bg, black fg
+export LESS_TERMCAP_us=$'\e[04;35m'     # start underline: underline magenta
+export LESS_TERMCAP_se=$reset_color     # end standout
+export LESS_TERMCAP_ue=$reset_color     # end underline
+export LESS_TERMCAP_me=$reset_color     # end bold/blink
 
 # Set LS_COLORS using (g)dircolors if found.
-local dircolors_cmds=(
-  $commands[dircolors](N) $commands[gdircolors](N)
-)
-if [[ -z "$LS_COLORS" ]] && (( $#dircolors_cmds )); then
-  cached-command "${dircolors_cmds[1]:t}" $dircolors_cmds[1] --sh
+if [[ -z "$LS_COLORS" ]]; then
+  if (( $+commands[dircolors] )); then
+    cached-command 'dircolors' dircolors --sh
+  elif (( $+commands[gdircolors] )); then
+    cached-command 'gdircolors' gdircolors --sh
+  else
+    export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+  fi
 fi
 
-# Missing dircolors is a good indicator of a BSD system.
-if (( ! $+commands[dircolors] )) || [[ "$OSTYPE" == darwin* ]]; then
+# Missing dircolors is a good indicator of a BSD system. Set LSCOLORS for macOS/BSD.
+if (( ! $+commands[dircolors] )); then
   # For BSD systems, set LSCOLORS
-  export CLICOLOR=1
+  export CLICOLOR=${CLICOLOR:-1}
   export LSCOLORS="${LSCOLORS:-exfxcxdxbxGxDxabagacad}"
-  # Also set LS_COLORS for good measure when gdircolors from coreutils is not installed.
-  export LS_COLORS="${LS_COLORS:-di=34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43}"
 fi
 
 # Set aliases.
@@ -54,7 +56,7 @@ if ! zstyle -t ':zephyr:plugin:color:alias' skip; then
   fi
 
   # Set colors for ls.
-  if (( ! $+commands[dircolors] )) || [[ "$OSTYPE" == darwin* ]]; then
+  if (( ! $+commands[dircolors] )) || is-macos; then
     alias ls="${aliases[ls]:-ls} -G"
   else
     alias ls="${aliases[ls]:-ls} --group-directories-first --color=auto"
@@ -65,4 +67,5 @@ fi
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # Mark this plugin as loaded.
-zstyle ":zephyr:plugin:color" loaded 'yes'
+zstyle ':zephyr:plugin:color' loaded 'yes'
+#endregion
