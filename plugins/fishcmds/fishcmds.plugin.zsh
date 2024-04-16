579e500ca3a1758a7b9c2ef3447cb5861a7c9325
mattmc3 <mattmc3@gmail.com>
2024-04-16 10:11:57 -0400

    fishcmds: Deprecate plugin

--- plugins/fishcmds/fishcmds.plugin.zsh
+++ plugins/fishcmds/fishcmds.plugin.zsh
@@ -2,10 +2,4 @@
 # fishcmds: Commands to make Fish users feel more at home in Zsh.
 #
 
-# Load plugin functions.
-0=${(%):-%N}
-fpath=(${0:a:h}/functions $fpath)
-autoload -Uz ${0:a:h}/functions/*(.:t)
-
-# Mark this plugin as loaded.
-zstyle ':zephyr:plugin:fishcmds' loaded 'yes'
+echo "Zephyr fishcmds deprecated. Functions moved elsewhere."
