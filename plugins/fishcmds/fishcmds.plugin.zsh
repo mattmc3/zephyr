3d6d3ff13987826b97e3868021c2550c72caee03
mattmc3 <mattmc3@gmail.com>
2024-07-13 12:57:41 -0400

    Move fish commands together

--- plugins/fishcmds/fishcmds.plugin.zsh
+++ plugins/fishcmds/fishcmds.plugin.zsh
@@ -2,4 +2,9 @@
 # fishcmds: Commands to make Fish users feel more at home in Zsh.
 #
 
-echo "Zephyr fishcmds deprecated. Functions moved elsewhere."
+# Bootstrap.
+0=${(%):-%N}
+zstyle -t ':zephyr:lib:bootstrap' loaded || source ${0:a:h:h:h}/lib/bootstrap.zsh
+
+# Add fish-like commands
+path=(${0:a:h}/bin $path)
