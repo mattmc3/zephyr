90245908d1e374a8c8a349aeb639c47aa2b11f60
mattmc3 <mattmc3@gmail.com>
2024-07-13 15:13:48 -0400

    Simplify

--- plugins/history/history.plugin.zsh
+++ plugins/history/history.plugin.zsh
@@ -11,23 +11,20 @@
 0=${(%):-%N}
 zstyle -t ':zephyr:lib:bootstrap' loaded || source ${0:a:h:h:h}/lib/bootstrap.zsh
 
-#region zephyr_plugin_history
-if ! zstyle -t ':zephyr:plugin:history:setopt' skip; then
-  # 16.2.4 History
-  setopt bang_hist               # Treat the '!' character specially during expansion.
-  setopt extended_history        # Write the history file in the ':start:elapsed;command' format.
-  setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
-  setopt hist_find_no_dups       # Do not display a previously found event.
-  setopt hist_ignore_all_dups    # Delete an old recorded event if a new event is a duplicate.
-  setopt hist_ignore_dups        # Do not record an event that was just recorded again.
-  setopt hist_ignore_space       # Do not record an event starting with a space.
-  setopt hist_reduce_blanks      # Remove extra blanks from commands added to the history list.
-  setopt hist_save_no_dups       # Do not write a duplicate event to the history file.
-  setopt hist_verify             # Do not execute immediately upon history expansion.
-  setopt inc_append_history      # Write to the history file immediately, not when the shell exits.
-  setopt NO_hist_beep            # Don't beep when accessing non-existent history.
-  setopt NO_share_history        # Don't share history between all sessions.
-fi
+# 16.2.4 History
+setopt bang_hist               # Treat the '!' character specially during expansion.
+setopt extended_history        # Write the history file in the ':start:elapsed;command' format.
+setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
+setopt hist_find_no_dups       # Do not display a previously found event.
+setopt hist_ignore_all_dups    # Delete an old recorded event if a new event is a duplicate.
+setopt hist_ignore_dups        # Do not record an event that was just recorded again.
+setopt hist_ignore_space       # Do not record an event starting with a space.
+setopt hist_reduce_blanks      # Remove extra blanks from commands added to the history list.
+setopt hist_save_no_dups       # Do not write a duplicate event to the history file.
+setopt hist_verify             # Do not execute immediately upon history expansion.
+setopt inc_append_history      # Write to the history file immediately, not when the shell exits.
+setopt NO_hist_beep            # Don't beep when accessing non-existent history.
+setopt NO_share_history        # Don't share history between all sessions.
 
 # Set the path to the history file.
 zstyle -s ':zephyr:plugin:history' histfile '_zhistfile'
@@ -52,10 +49,8 @@ zstyle -s ':zephyr:plugin:history' histsize 'HISTSIZE' \
   || HISTSIZE=20000
 
 # Set Zsh aliases related to history.
-if ! zstyle -t ':zephyr:plugin:history:alias' skip; then
-  alias hist='fc -li'
-  alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
-fi
+alias hist='fc -li'
+alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -nr | head"
 
 # Mark this plugin as loaded.
 zstyle ':zephyr:plugin:history' loaded 'yes'
