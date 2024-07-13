686c0a85b205e63df892f259fbe0afdfc1770b3b
mattmc3 <mattmc3@gmail.com>
2024-07-13 12:30:03 -0400

    Simplify directory

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -10,15 +10,15 @@
 0=${(%):-%N}
 zstyle -t ':zephyr:lib:bootstrap' loaded || source ${0:a:h:h:h}/lib/bootstrap.zsh
 
-# 16.2.3 Expansion and Globbing
-setopt extended_glob           # Use more awesome globbing features.
-setopt glob_dots               # Include dotfiles when globbing.
-
 # 16.2.1 Changing Directories
 setopt auto_pushd              # Make cd push the old directory onto the dirstack.
 setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
 setopt pushd_silent            # Do not print the directory stack after pushd or popd.
 setopt pushd_to_home           # Push to home directory when no argument is given.
+
+# 16.2.3 Expansion and Globbing
+setopt extended_glob           # Use more awesome globbing features.
+setopt glob_dots               # Include dotfiles when globbing.
 
 # 16.2.6 Input/Output
 setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
@@ -28,32 +28,28 @@ setopt NO_rm_star_silent       # Ask for confirmation for `rm *' or `rm path/*'
 setopt multios                 # Write to multiple descriptors.
 
 # Set directory aliases.
-if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
-  alias -- -='cd -'
-  alias dirh='dirs -v'
-  () {
-    local i dotdots=".."
-    for i in {1..9}; do
-      alias "$i"="cd -${i}"       # dirstack aliases (eg: "3"="cd -3")
-      alias -g "..$i"="$dotdots"  # backref aliases (eg: "..3"="../../..")
-      dotdots+='/..'
-    done
-  }
-fi
+alias -- -='cd -'
+alias dirh='dirs -v'
+() {
+  local i dotdots=".."
+  for i in {1..9}; do
+    alias "$i"="cd -${i}"       # dirstack aliases (eg: "3"="cd -3")
+    alias -g "..$i"="$dotdots"  # backref aliases (eg: "..3"="../../..")
+    dotdots+='/..'
+  done
+}
 
-if ! zstyle -t ':zephyr:plugin:directory:function' skip; then
-  ##? up: Quickly go up any number of directories.
-  function up {
-    local parents=${1:-1}
-    if ! [[ "$parents" -gt 0 ]]; then
-      echo >&2 "up: expecting numeric argument: number of parent dirs"
-      return 1
-    fi
-    local -a dotdots=($(printf "..%.0s\n" {1..$parents}))
-    local cdstr="${(pj:/:)dotdots}"
-    cd $cdstr
-  }
-fi
+##? up: Quickly go up any number of directories.
+function up {
+  local parents=${1:-1}
+  if ! [[ "$parents" -gt 0 ]]; then
+    echo >&2 "up: expecting numeric argument: number of parent dirs"
+    return 1
+  fi
+  local -a dotdots=($(printf "..%.0s\n" {1..$parents}))
+  local cdstr="${(pj:/:)dotdots}"
+  cd $cdstr
+}
 
 # Mark this plugin as loaded.
 zstyle ':zephyr:plugin:directory' loaded 'yes'
