544b217c3f1fb69f28c3a06b169caa16e4e494f5
mattmc3 <mattmc3@gmail.com>
2024-08-06 20:42:46 -0400

    Update directory plugin

--- plugins/directory/README.md
+++ plugins/directory/README.md
@@ -43,7 +43,13 @@ This plugin sets no variables.
 
 ## Customizations
 
-This plugin does not have any zstyles for customization.
+To skip setting directory related aliases, you can set:
+
+`zstyle ':zephyr:plugin:directory:alias' 'skip' 'yes'`
+
+To always skip aliases, you can set:
+
+`zstyle ':zephyr:plugin:*:alias' 'skip' 'yes'`
 
 [16.2.1]: https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories
 [16.2.3]: https://zsh.sourceforge.io/Doc/Release/Options.html#Expansion-and-Globbing
--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -2,43 +2,34 @@
 # directory: Set features related to Zsh directories and dirstack.
 #
 
-# References:
-# - https://github.com/sorin-ionescu/prezto/tree/master/modules/directory
-# - https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/directories.zsh
-# - https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories
-
-# 16.2.1 Changing Directories
-setopt auto_pushd              # Make cd push the old directory onto the dirstack.
-setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
-setopt pushd_silent            # Do not print the directory stack after pushd or popd.
-setopt pushd_to_home           # Push to home directory when no argument is given.
-
-# 16.2.3 Expansion and Globbing
-setopt extended_glob           # Use extended chars (#,~,^) in globbing patterns.
-setopt glob_dots               # Include dotfiles when globbing.
-
-# 16.2.6 Input/Output
-setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
-setopt NO_rm_star_silent       # Ask for confirmation for `rm *' or `rm path/*'
-
-# 16.2.9 Scripts and Functions
-setopt multios                 # Write to multiple descriptors.
+# Set Zsh options related to directories, globbing, and I/O.
+setopt auto_pushd         # Make cd push the old directory onto the dirstack.
+setopt pushd_minus        # Exchanges meanings of +/- when navigating the dirstack.
+setopt pushd_silent       # Do not print the directory stack after pushd or popd.
+setopt pushd_to_home      # Push to home directory when no argument is given.
+setopt multios            # Write to multiple descriptors.
+setopt extended_glob      # Use extended globbing syntax (#,~,^).
+setopt glob_dots          # Don't hide dotfiles from glob patterns.
+setopt NO_clobber         # Don't overwrite files with >. Use >| to bypass.
+setopt NO_rm_star_silent  # Ask for confirmation for `rm *' or `rm path/*'
 
 # Set directory aliases.
-alias -- -='cd -'
-alias dirh='dirs -v'
-() {
-  local i dotdots=".."
-  for i in {1..9}; do
-    alias "$i"="cd -${i}"       # dirstack aliases (eg: "3"="cd -3")
-    alias -g "..$i"="$dotdots"  # backref aliases (eg: "..3"="../../..")
-    dotdots+='/..'
-  done
-}
+if ! zstyle -t ':zephyr:plugin:directory:alias' skip; then
+  alias -- -='cd -'
+  alias dirh='dirs -v'
 
-##? up: Quickly go up any number of directories.
+  _dotdot=".."
+  for _index in {1..9}; do
+    alias "$_index"="cd -${_index}"      # dirstack aliases (eg: "2"="cd -2")
+    alias -g "..${_index}"="${_dotdot}"  # backref aliases (eg: "..3"="../../..")
+    _dotdot+="/.."
+  done
+  unset _dotdot _index
+fi
+
+# Quickly go up any number of directories.
 function up {
-  local parents=${1:-1}
+  local parents="${1:-1}"
   if [[ ! "$parents" -gt 0 ]]; then
     echo >&2 "usage: up [<num>]"
     return 1
@@ -47,7 +38,7 @@ function up {
   while (( --parents )); do
     dotdots+="/.."
   done
-  cd $dotdots
+  cd "$dotdots"
 }
 
 # Mark this plugin as loaded.
