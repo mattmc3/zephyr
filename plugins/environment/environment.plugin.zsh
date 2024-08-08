78904556083e24ec00b295906c4f36a75a3abeae
mattmc3 <mattmc3@gmail.com>
2024-08-08 15:52:46 -0400

    environment: Cleanup

--- plugins/environment/environment.plugin.zsh
+++ plugins/environment/environment.plugin.zsh
@@ -11,29 +11,25 @@
 zstyle -t ':zephyr:lib:bootstrap' loaded || source ${0:a:h:h:h}/lib/bootstrap.zsh
 #endregion
 
-# 16.2.3 Expansion and Globbing
-setopt extended_glob           # Use extended chars (#,~,^) in globbing patterns.
+# Set Zsh options related to globbing.
+setopt extended_glob         # Use more awesome globbing features.
+setopt glob_dots             # Include dotfiles when globbing.
+setopt NO_rm_star_silent     # Ask for confirmation for `rm *' or `rm path/*'
 
-# 16.2.6 Input/Output
-setopt interactive_comments    # Enable comments in interactive shell.
-setopt rc_quotes               # Allow 'Hitchhikers''s Guide' instead of 'Hitchhikers'\''s Guide'.
-setopt NO_flow_control         # Disable start/stop characters (usually ^Q/^S) in shell editor.
-setopt NO_mail_warning         # Don't print a warning message if a mail file has been accessed.
+# Set general Zsh options.
+setopt combining_chars       # Combine 0-len chars with the base character (eg: accents).
+setopt interactive_comments  # Enable comments in interactive shell.
+setopt rc_quotes             # Allow 'Hitchhikers''s Guide' instead of 'Hitchhikers'\''s Guide'.
+setopt NO_mail_warning       # Don't print a warning message if a mail file has been accessed.
+setopt NO_beep               # Don't beep on error in line editor.
 
-# 16.2.7 Job Control
-setopt auto_resume             # Attempt to resume existing job before creating a new process.
-setopt long_list_jobs          # List jobs in the long format by default.
-setopt notify                  # Report status of background jobs immediately.
-setopt NO_bg_nice              # Don't run all background jobs at a lower priority.
-setopt NO_check_jobs           # Don't report on jobs when shell exit.
-setopt NO_hup                  # Don't kill jobs on shell exit.
-
-# 16.2.12 Zle
-setopt combining_chars         # Combine 0-len chars with the base character (eg: accents).
-setopt NO_beep                 # Do not beep on error in line editor.
-
-# Allow mapping Ctrl+S and Ctrl+Q shortcuts
-[[ -r ${TTY:-} && -w ${TTY:-} && $+commands[stty] == 1 ]] && stty -ixon <$TTY >$TTY
+# Set Zsh options related to job control.
+setopt auto_resume           # Attempt to resume existing job before creating a new process.
+setopt long_list_jobs        # List jobs in the long format by default.
+setopt notify                # Report status of background jobs immediately.
+setopt NO_bg_nice            # Don't run all background jobs at a lower priority.
+setopt NO_check_jobs         # Don't report on jobs when shell exit.
+setopt NO_hup                # Don't kill jobs on shell exit.
 
 # Set XDG base dirs.
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
@@ -42,9 +38,7 @@ if zstyle -T ':zephyr:plugin:environment' use-xdg-basedirs; then
   export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
   export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
   export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
-  () {
-    local _zdir; for _zdir in $@; [ -d ${(P)_zdir} ] || mkdir -p ${(P)_zdir}
-  } XDG_{CONFIG,CACHE,DATA,STATE}_HOME
+  mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME $XDG_STATE_HOME
 fi
 
 # Editors
@@ -59,24 +53,6 @@ fi
 
 # Set language.
 export LANG=${LANG:-en_US.UTF-8}
-
-# Ensure path arrays do not contain duplicates.
-typeset -gU cdpath fpath mailpath path
-
-# Set the list of directories that Zsh searches for programs.
-if [[ ! -v prepath ]]; then
-  # If path ever gets out of order, you can use `path=($prepath $path)` to reset it.
-  typeset -ga prepath=(
-    $HOME/{,s}bin(N)
-    $HOME/.local/{,s}bin(N)
-  )
-fi
-path=(
-  $prepath
-  /opt/{homebrew,local}/{,s}bin(N)
-  /usr/local/{,s}bin(N)
-  $path
-)
 
 # Set the default Less options.
 # Mouse-wheel scrolling can be disabled with -X (disable screen clearing).
@@ -101,6 +77,29 @@ fi
 
 # Use `< file` to quickly view the contents of any file.
 [[ -z "$READNULLCMD" ]] || READNULLCMD=$PAGER
+
+# Ensure path arrays do not contain duplicates.
+typeset -gU cdpath fpath mailpath path
+
+# Add /usr/local/bin to path.
+path=(/usr/local/{,s}bin(N) $path)
+
+# Set the list of directories that Zsh searches for programs.
+if [[ ! -v prepath ]]; then
+  typeset -ga prepath
+  # If path ever gets out of order, you can use `path=($prepath $path)` to reset it.
+  zstyle -a ':zephyr:plugin:environment' 'prepath' 'prepath' \
+  || prepath=(
+    $HOME/{,s}bin(/N)
+    $HOME/.local/{,s}bin(/N)
+  )
+fi
+path=(
+  $prepath
+  /opt/{homebrew,local}/{,s}bin(/N)
+  /usr/local/{,s}bin(/N)
+  $path
+)
 
 #region MARK LOADED
 zstyle ':zephyr:plugin:environment' loaded 'yes'
