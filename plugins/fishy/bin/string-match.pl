3aec72604ccb5bd4eb50b55be74d52d874a3dbf8
mattmc3 <mattmc3@gmail.com>
2025-10-28 21:48:55 -0400

    Evolve string-match code

--- plugins/fishy/bin/string-match.pl
+++ plugins/fishy/bin/string-match.pl
@@ -3,6 +3,26 @@ use strict;
 use warnings;
 use Getopt::Long qw(:config no_ignore_case bundling);
 
+# Simple options class
+package MatchOptions {
+    sub new {
+        my ($class, %args) = @_;
+        return bless \%args, $class;
+    }
+
+    sub all         { $_[0]->{all} }
+    sub entire      { $_[0]->{entire} }
+    sub groups_only { $_[0]->{groups_only} }
+    sub help        { $_[0]->{help} }
+    sub ignore_case { $_[0]->{ignore_case} }
+    sub index       { $_[0]->{index} }
+    sub invert      { $_[0]->{invert} }
+    sub quiet       { $_[0]->{quiet} }
+    sub regex       { $_[0]->{regex} }
+}
+
+package main;
+
 sub usage {
     print "string match [-a | --all] [-e | --entire] [-i | --ignore-case]\n";
     print "             [-g | --groups-only] [-r | --regex] [-n | --index]\n";
@@ -14,6 +34,80 @@ sub die_err {
     my ($code, $msg) = @_;
     print STDERR "$msg\n";
     exit $code;
+}
+
+# Print capture groups, filtering out undefined values
+sub print_groups {
+    my (@groups) = @_;
+    print "$_\n" for grep { defined } @groups;
+}
+
+# Print match results based on options
+sub print_results {
+    my ($str, $result_ref, $indices_ref, $pattern, $opts) = @_;
+    my @result = @$result_ref;
+
+    # With --invert or --entire, print the entire string
+    if ($opts->invert || $opts->entire) {
+        if ($opts->index) {
+            print "1 " . length($str) . "\n";
+        } else {
+            print "$str\n";
+        }
+        # With --entire (but not --invert), also print capture groups
+        if ($opts->entire && !$opts->invert && @result > 1 && !$opts->groups_only) {
+            my (undef, @matches) = @result;
+            print_groups(@matches);
+        }
+        return 1;
+    }
+
+    # Handle --index
+    if ($opts->index) {
+        if ($opts->all) {
+            my @indices = do_match($str, $pattern, $opts->ignore_case, 1, 1);
+            print "$_\n" for @indices;
+        } else {
+            print "$_\n" for @$indices_ref;
+        }
+        return 1;
+    }
+
+    # Handle --groups-only
+    if ($opts->groups_only) {
+        my @matches;
+        if ($opts->all) {
+            @matches = do_match($str, $pattern, $opts->ignore_case, 1, 0);
+        } elsif (@result > 1) {
+            (undef, @matches) = @result;
+        }
+
+        @matches = grep { defined && length } @matches;
+
+        if (@matches) {
+            print "$_\n" for @matches;
+            return 1;
+        } else {
+            return 0;  # No valid capture groups
+        }
+    }
+
+    # Handle capture groups
+    if (@result > 1) {
+        my ($full_match, @matches) = @result;
+        print "$full_match\n" if defined $full_match;
+        print_groups(@matches);
+        return 1;
+    }
+
+    # Handle regular matches
+    if ($opts->all) {
+        my @matches = do_match($str, $pattern, $opts->ignore_case, 1, 0);
+        print "$_\n" for @matches;
+    } else {
+        print "$result[0]\n" if defined $result[0];
+    }
+    return 1;
 }
 
 # Convert glob pattern to regex
@@ -45,7 +139,20 @@ sub do_match {
             }
             return @results;
         } else {
-            return $str =~ /$re/g;
+            # For global matching, return all matches (or capture groups if present)
+            my @results = ();
+            while ($str =~ /$re/g) {
+                # If there are capture groups, collect them
+                if ($#- > 0) {
+                    for my $i (1 .. $#-) {
+                        push @results, substr($str, $-[$i], $+[$i] - $-[$i]);
+                    }
+                } else {
+                    # No capture groups, just the full match
+                    push @results, $&;
+                }
+            }
+            return @results;
         }
     } elsif ($str =~ /$re/) {
         # Return full match plus any capture groups
@@ -71,37 +178,28 @@ sub do_match {
 }
 
 # Parse options
-my ($opt_all, $opt_entire, $opt_help, $opt_ignore_case, $opt_groups_only);
-my ($opt_regex, $opt_index, $opt_invert, $opt_quiet);
+my $opts = MatchOptions->new();
 
 GetOptions(
-    'a|all'         => \$opt_all,
-    'e|entire'      => \$opt_entire,
-    'g|groups-only' => \$opt_groups_only,
-    'h|help'        => \$opt_help,
-    'i|ignore-case' => \$opt_ignore_case,
-    'n|index'       => \$opt_index,
-    'q|quiet'       => \$opt_quiet,
-    'r|regex'       => \$opt_regex,
-    'v|invert'      => \$opt_invert,
+    'a|all'         => \$opts->{all},
+    'e|entire'      => \$opts->{entire},
+    'g|groups-only' => \$opts->{groups_only},
+    'h|help'        => \$opts->{help},
+    'i|ignore-case' => \$opts->{ignore_case},
+    'n|index'       => \$opts->{index},
+    'q|quiet'       => \$opts->{quiet},
+    'r|regex'       => \$opts->{regex},
+    'v|invert'      => \$opts->{invert},
 ) or die_err(2, "string match: invalid option");
 
 # Act on help flag
-if ($opt_help) {
+if ($opts->help) {
     usage();
     exit 0;
 }
 
 # Check for unimplemented options
-die_err(1, "-g/--groups-only option not yet implemented.") if $opt_groups_only;
-
-# Collect piped input if present
-if (!-t STDIN) {
-    while (my $line = <STDIN>) {
-        chomp $line;
-        push @ARGV, $line;
-    }
-}
+# (none currently)
 
 # Must have at least a pattern
 die_err(2, "string match: missing PATTERN.") if @ARGV == 0;
@@ -109,60 +207,56 @@ die_err(2, "string match: missing PATTERN.") if @ARGV == 0;
 my $pattern = shift @ARGV;
 
 # Convert glob to regex if not using --regex
-$pattern = glob_to_regex($pattern) unless $opt_regex;
+if (!$opts->regex) {
+    $pattern = glob_to_regex($pattern);
+    # Without --entire, anchor the pattern to match whole string
+    $pattern = "^${pattern}\$" unless $opts->entire;
+}
 
-# If no strings provided, nothing to match
-exit 1 if @ARGV == 0;
+# Determine input source: piped stdin or command line args
+my $use_stdin = (!-t STDIN && @ARGV == 0);
+
+# If no strings provided and no stdin, nothing to match
+exit 1 if !$use_stdin && @ARGV == 0;
 
 # Process each string
 my $match_found = 0;
 
-foreach my $str (@ARGV) {
+# Set up input iterator
+my $get_next_str = $use_stdin
+    ? sub { my $line = <STDIN>; chomp $line if defined $line; return $line; }
+    : sub { return shift @ARGV; };
+
+while (defined(my $str = $get_next_str->())) {
     # Apply the pattern match
     my @result;
     my $indices_ref;
 
-    if ($opt_index) {
-        ($result[0], $indices_ref) = do_match($str, $pattern, $opt_ignore_case, 0, 1);
+    if ($opts->index && !$opts->invert) {
+        ($result[0], $indices_ref) = do_match($str, $pattern, $opts->ignore_case, 0, 1);
         @result = @{$result[0]} if $result[0];
     } else {
-        @result = do_match($str, $pattern, $opt_ignore_case, 0, 0);
+        @result = do_match($str, $pattern, $opts->ignore_case, 0, 0);
     }
 
     # Handle --invert
     my $matched = @result ? 1 : 0;
-    $matched = !$matched if $opt_invert;
+    $matched = !$matched if $opts->invert;
 
     next unless $matched;
 
     $match_found = 1;
-    next if $opt_quiet;
 
-    # With --invert or --entire, print the entire string
-    if ($opt_invert || $opt_entire) {
-        print "$str\n";
-    } elsif ($opt_index) {
-        # Print indices instead of matches
-        if ($opt_all) {
-            # For --all with --index, get all match positions
-            my @indices = do_match($str, $pattern, $opt_ignore_case, 1, 1);
-            print "$_\n" for @indices;
-        } else {
-            print "$_\n" for @{$indices_ref};
-        }
-    } elsif (@result > 1) {
-        # Pattern has capture groups - print full match then groups
-        my ($full_match, @matches) = @result;
-        print "$full_match\n" if defined $full_match;
-        print "$_\n" for grep { defined } @matches;
-    } else {
-        # No capture groups - print matched portion(s)
-        if ($opt_all) {
-            my @matches = do_match($str, $pattern, $opt_ignore_case, 1, 0);
-            print "$_\n" for @matches;
-        } else {
-            print "$result[0]\n" if defined $result[0];
-        }
+    # With --quiet, exit immediately on first match
+    exit 0 if $opts->quiet;
+
+    # Print results and check if we actually printed anything (for --groups-only)
+    my $printed = print_results($str, \@result, $indices_ref, $pattern, $opts);
+
+    # If nothing was printed (e.g., no valid capture groups), don't count as match
+    if (!$printed) {
+        $match_found = 0;
+        next;
     }
 }
 
--- plugins/fishy/tests/test_string_match.md
+++ plugins/fishy/tests/test_string_match.md
@@ -89,6 +89,133 @@ $ string match -r -i "0x[0-9a-f]{1,8}" "int magic = 0xBadC0de;"
 $
 ```
 
+```sh
+$ # From https://github.com/fish-shell/fish-shell/issues/5201
+$ # 'string match -r with empty capture groups'
+$ string match -r '^([ugoa]*)([=+-]?)([rwx]*)$' '=r'
+=r
+
+=
+r
+$ ### Test some failure cases
+$ (set -o pipefail; string match -r "[" "a[sd" 2>&1 | cut -d';' -f1) && echo "unexpected exit 0"
+Unmatched [ in regex
+$
+```
+
+```sh
+$ string match -r -v "[dcantg].*" dog can cat diz || echo "no regexp invert match"
+no regexp invert match
+$ string match -v "*" dog can cat diz || echo "no glob invert match"
+no glob invert match
+$ string match -rvn a bbb || echo "exit 1"
+1 3
+$
+```
+
+```sh
+$ # Test equivalent matches with/without the --entire, --regex, and --invert flags.
+$ string match -e x abc dxf xyz jkx x z || echo exit 1
+dxf
+xyz
+jkx
+x
+$ string match x abc dxf xyz jkx x z
+x
+$ string match --entire -r "a*b[xy]+" abc abxc bye aaabyz kaabxz abbxy abcx caabxyxz || echo exit 1
+abxc
+bye
+aaabyz
+kaabxz
+abbxy
+caabxyxz
+$
+$ # 'string match --entire "" -- banana'
+$ string match --entire "" -- banana || echo exit 1
+banana
+$
+$ # 'string match -r "a*b[xy]+" abc abxc bye aaabyz kaabxz abbxy abcx caabxyxz'
+$ string match -r "a*b[xy]+" abc abxc bye aaabyz kaabxz abbxy abcx caabxyxz || echo exit 1
+abx
+by
+aaaby
+aabx
+bxy
+aabxyx
+$
+$ # Make sure that groups are handled correct with/without --entire.
+$ # 'string match --entire -r "a*b([xy]+)" abc abxc bye aaabyz kaabxz abbxy abcx caabxyxz'
+$ string match --entire -r "a*b([xy]+)" abc abxc bye aaabyz kaabxz abbxy abcx caabxyxz || echo exit 1
+abxc
+x
+bye
+y
+aaabyz
+y
+kaabxz
+x
+abbxy
+xy
+caabxyxz
+xyx
+$
+$ # 'string match -r "a*b([xy]+)" abc abxc bye aaabyz kaabxz abbxy abcx caabxyxz'
+$ string match -r "a*b([xy]+)" abc abxc bye aaabyz kaabxz abbxy abcx caabxyxz || echo exit 1
+abx
+x
+by
+y
+aaaby
+y
+aabx
+x
+bxy
+xy
+aabxyx
+xyx
+$
+```
+
+More tests
+
+```sh
+$ string match -eq asd asd; echo $?
+0
+$ # --quiet should quit early
+$ echo "Checking that --quiet quits early - if this is broken it hangs"
+Checking that --quiet quits early - if this is broken it hangs
+$ yes | string match -q y; echo $?
+0
+$
+```
+
+```sh
+$ string match -rg '(.*)fish' catfish
+cat
+$ string match -rg '(.*)fish' shellfish
+shell
+$ # An empty match
+$ string match -rg '(.*)fish' fish
+$ # No match at all
+$ string match -rg '(.*)fish' banana
+$ # Make sure it doesn't start matching something
+$ string match -r --groups-only '(.+)fish' fish; echo $?
+1
+$ # Multiple groups
+$ string match -r --groups-only '(.+)fish(.*)' catfishcolor
+cat
+color
+$
+$ # Examples specifically called out in #6056.
+$ echo "foo bar baz" | string match -rg 'foo (bar) baz'
+bar
+$ echo "foo1x foo2x foo3x" | string match -arg 'foo(\d)x'
+1
+2
+3
+$
+```
+
 Teardown
 
 ```sh
